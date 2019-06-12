a03e7ec4cc9257c581a1e7e2de8966a425b9efdd
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-06-11 19:03:45 -0700

    basic asyncio aiohttp example enhanced with colorama to help show simultanaeity

--- asyncio_aiohttp_ex.py
+++ asyncio_aiohttp_ex.py
@@ -4,21 +4,67 @@
 
 import aiohttp
 import asyncio
+import colorama
+import bs4
+
+targ1 = {
+    'name': 'Jimmy Gizmo Website - Ninth Device',
+    'code': 'NtDv',
+    'url': 'http://www.ninthdevice.com'
+}
+
+targ2 = {
+    'name': 'SF Gate News Site',
+    'code': 'SfGt',
+    'url': 'http://www.sfgate.com/'
+}
+
+targ3 = {
+    'name': 'Ninth Device',
+    'code': 'NtDv',
+    'url': 'http://www.ninthdevice.com'
+}
+
+targets = [targ1, targ2, targ3]
+
+one_target = targ2
 
 
-async def fetch(session, url):
+async def fetch(session: aiohttp.ClientSession, url: str) -> str:
+    print(colorama.Fore.MAGENTA + "fetch: aiohttp issuing HTTP request", flush=True)
     async with session.get(url) as response:
         return await response.text()
 
 
-async def main():
+def get_title(html: str) -> str:
+    print(colorama.Fore.CYAN + f"Extracting TITLE from the HTML", flush=True)
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    title = soup.find('title')
+    if not title:
+        return '(NO TITLE)'
+
+    return title.text.strip()
+
+
+async def process_target(target: dict):
+    print(colorama.Fore.YELLOW + f"HTTP Async Get request for target: {target['code']}", flush=True)
+    print(colorama.Fore.YELLOW + f"URL: {target['url']}", flush=True)
+
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://www.ninthdevice.com')
-        print(html)
+        html = await fetch(session, target['url'])
+        title = get_title(html)
+        print(colorama.Fore.YELLOW + f"{target['code']}: {title}", flush=True)
+
+
+
+async def main(targets: list):
+    pass
+    # TODO: Next will process many targets at once
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(process_target(one_target))
+
 
 
 ##
