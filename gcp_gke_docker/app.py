4592f3fea02499aaa6b06e337ca5671806f3776c
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-06-28 01:30:19 -0700

    good basic flask container - hello world on port 80 - ready to test gke cluster

--- gcp_gke_docker/app.py
+++ gcp_gke_docker/app.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Flask inside a Docker container. Container: python:3.4"
+    html = "<h3>Flask via Docker. Container: python:3.7-alpine</h3>"
+    return html
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    #app.run(debug=True, host="0.0.0.0")
+    app.run(debug = True, host = "0.0.0.0", port = 80)
+
