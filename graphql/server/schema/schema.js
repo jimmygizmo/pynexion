af373c155e505268b77b52bed1d5d5c4d5a35b29
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-07-13 01:16:48 -0700

    author now supported at basic level but only singular queries supported.

--- graphql/server/schema/schema.js
+++ graphql/server/schema/schema.js
@@ -8,7 +8,8 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt
 } = graphql;
 
 // test data to use prior to implementing MongoDB
@@ -18,12 +19,27 @@ var books = [
     {name: 'Book Title 3', genre: 'Sci-Fi', id: '3'},
 ];
 
+var authors = [
+    {name: 'Bob Jamonevitch', age: 44, id: '1'},
+    {name: 'Melvin McGill', age: 42, id: '2'},
+    {name: 'Larry Boonsmith', age: 66, id: '3'},
+];
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
     })
 });
 
@@ -38,8 +54,21 @@ const RootQuery = new GraphQLObjectType({
                 // is the type stored in the array we run _.find against.
                 // TODO: Clarify exactly how that works. In video this is at
                 // 1:02-1:03.
+                // NOTE: Clarification is provided by reading the definition
+                // of the GraphQLID type in the graphiql utility right side
+                // window. Basically, it accepts String or Int for input but
+                // treats both as a String. This definition is in the context
+                // of results data but the inverse can be assumed for queries,
+                // which is the context here. Best explanation I got so far.
                 //console.log(typeof(args.id))
                 return _.find(books, {id: args.id});
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                return _.find(authors, {id: args.id});
             }
         }
     }
