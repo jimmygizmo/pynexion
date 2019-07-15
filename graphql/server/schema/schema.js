984695f8c6c8f04eae5068b570ab6ec1f5d211af
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-07-15 00:40:50 -0700

    disabled dummy data code. added graphql code for first mutation to create authors. works good - verified in compass.

--- graphql/server/schema/schema.js
+++ graphql/server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../models/book')
+const Author = require('../models/author')
 
 // The below syntax is called 'destructuring' and sort of grabs the various
 // objects in the curly brackets out of the namespace, graphql in
@@ -14,20 +16,20 @@ const {
 } = graphql;
 
 // test data to use prior to implementing MongoDB
-var books = [
-    {name: 'Book Title 1', genre: 'Fantasy', id: '1', authorid: '1'},
-    {name: 'Book Title 2', genre: 'Fantasy', id: '2', authorid: '2'},
-    {name: 'Book Title 3', genre: 'Sci-Fi', id: '3', authorid: '3'},
-    {name: 'Book Title 4', genre: 'Fantasy', id: '1', authorid: '2'},
-    {name: 'Book Title 5', genre: 'Fantasy', id: '2', authorid: '3'},
-    {name: 'Book Title 6', genre: 'Fantasy', id: '3', authorid: '3'},
-];
+// var books = [
+//     {name: 'Book Title 1', genre: 'Fantasy', id: '1', authorid: '1'},
+//     {name: 'Book Title 2', genre: 'Fantasy', id: '2', authorid: '2'},
+//     {name: 'Book Title 3', genre: 'Sci-Fi', id: '3', authorid: '3'},
+//     {name: 'Book Title 4', genre: 'Fantasy', id: '1', authorid: '2'},
+//     {name: 'Book Title 5', genre: 'Fantasy', id: '2', authorid: '3'},
+//     {name: 'Book Title 6', genre: 'Fantasy', id: '3', authorid: '3'},
+// ];
 
-var authors = [
-    {name: 'Bob Jamonevitch', age: 44, id: '1'},
-    {name: 'Melvin McGill', age: 42, id: '2'},
-    {name: 'Larry Boonsmith', age: 66, id: '3'},
-];
+// var authors = [
+//     {name: 'Bob Jamonevitch', age: 44, id: '1'},
+//     {name: 'Melvin McGill', age: 42, id: '2'},
+//     {name: 'Larry Boonsmith', age: 66, id: '3'},
+// ];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -38,8 +40,8 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent, args){
-                console.log(parent);
-                return _.find(authors, {id: parent.authorid});
+                // console.log(parent);
+                // return _.find(authors, {id: parent.authorid});
 
             }
         }
@@ -55,9 +57,8 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args){
-                return _.filter(books, {authorid: parent.id});
+                // return _.filter(books, {authorid: parent.id});
             }
-            ////////////////////////
         }
     })
 });
@@ -69,44 +70,55 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args){
-                // Shows that args.id is always string at this point which
-                // is the type stored in the array we run _.find against.
-                // TODO: Clarify exactly how that works. In video this is at
-                // 1:02-1:03.
-                // NOTE: Clarification is provided by reading the definition
-                // of the GraphQLID type in the graphiql utility right side
-                // window. Basically, it accepts String or Int for input but
-                // treats both as a String. This definition is in the context
-                // of results data but the inverse can be assumed for queries,
-                // which is the context here. Best explanation I got so far.
-                //console.log(typeof(args.id))
-                return _.find(books, {id: args.id});
+                // return _.find(books, {id: args.id});
             }
         },
         author: {
             type: AuthorType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args){
-                return _.find(authors, {id: args.id});
+                // return _.find(authors, {id: args.id});
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args){
-                return books;
+                // return books;
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args){
-                return authors;
+                // return authors;
             }
         }
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parent, args){
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
+            }
+        }
+    }
+});
+
+
 module.exports = new GraphQLSchema({
     // initial root query
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
 
