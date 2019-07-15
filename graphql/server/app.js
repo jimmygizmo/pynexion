38a35861fc65cbb10e04e0de2f05fae4ca81b991
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-07-14 22:47:33 -0700

    .env file support via dotenv added. database cred composition in place. secrets will not go to repo. db conn working.

--- graphql/server/app.js
+++ graphql/server/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+// Initial usage of dotenv - Do we have any reason for a const var here?
+require('dotenv').config();
+//console.log(process.env);
 
 const app = express();
+
+const dbpass = process.env.REACT_APP_DB_PASS;
+const dbuser = process.env.REACT_APP_DB_USER;
+const dbhost = process.env.REACT_APP_DB_HOST;
+
+var connstr = 'mongodb+srv://';
+connstr += dbuser + ':' + dbpass;
+connstr += '@' + dbhost;
+connstr += '/test?retryWrites=true&w=majority';
+//console.log(connstr);
+
+mongoose.connect(connstr);
+mongoose.connection.once('open', () => {
+    console.log('connected to DB');
+})
 
 //app.use('/graphql', graphqlHTTP({  OPTIONS-GO-HERE  }));
 app.use('/graphql', graphqlHTTP({
