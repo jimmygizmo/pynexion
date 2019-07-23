a9bb4144244b11b6711d838c684f517c7e51e16c
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-07-23 01:49:35 -0700

    moved queries to ext file. started mutation to add book. modified addbook component closer to using mutation

--- graphql/client/src/components/AddBook.js
+++ graphql/client/src/components/AddBook.js
@@ -1,50 +1,58 @@
 import React, { Component } from 'react';
-import { gql } from 'apollo-boost';
-import { graphql } from 'react-apollo';
+//import { gql } from 'apollo-boost';
+import { graphql, compose } from 'react-apollo';
+import { getAuthorsQuery, addBookMutation } from '../queries/queries'
 
-const getAuthorsQuery = gql`
-    {
-        authors {
-            name
-            id
-        }
-    }
-`
 
 class AddBook extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "",
+            genre: "",
+            authorid: ""
+        }
+    }
+
     displayAuthors(){
-        var data = this.props.data
+        var data = this.props.getAuthorsQuery
+        //console.log(this.props);
         if (data.loading) {
             return( <option disabled>Loading authors...</option> );
         } else {
             return data.authors.map(author => {
                 return(
-                    <option key={author.id}>{author.name}</option>
+                    <option key={author.id} value={author.id}>{author.name}</option>
                 );
             });  
         }
+    }
+
+    submitForm(e){
+        e.preventDefault();
+        console.log(this.state);
     }
 
     render() {
         //console.log(this.props);
 
         return (
-            <form id="add-book">
+            <form id="add-book" onSubmit={ this.submitForm.bind(this)}>
 
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text" />
+                    <input type="text" onChange={ (e) => this.setState({name: e.target.value})} />
                 </div>
 
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text" />
+                    <input type="text" onChange={ (e) => this.setState({genre: e.target.value})} />
                 </div>
 
                 <div className="field">
                     <label>Author:</label>
-                    <select>
+                    <select onChange={ (e) => this.setState({authorid: e.target.value})}>
                         <option>Select Author</option>
                         {this.displayAuthors()}
                     </select>
@@ -58,5 +66,9 @@ class AddBook extends Component {
 
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+
+export default compose(
+    graphql(getAuthorsQuery, {name: "getAuthorsQuery"}),
+    graphql(addBookMutation, {name: "addBookMutation"})
+)(AddBook);
 
---
+++ graphql/client/src/queries/queries.js
@@ -0,0 +1,34 @@
+import { gql } from 'apollo-boost';
+
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`
+
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            name
+            id
+        }
+    }
+`
+
+
+const addBookMutation = gql`
+    mutation {
+        addBook(name: "", genre: "", authorid: ""){
+            name
+            id
+        }
+    }
+`
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation };
+
