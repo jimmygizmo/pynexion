3f6134d426e8620b56a74f297b9a35343ec643ac
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-07-26 01:41:30 -0700

    book list refreshes now after an addbook mutation

--- graphql/client/src/components/AddBook.js
+++ graphql/client/src/components/AddBook.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import { gql } from 'apollo-boost';
 import { graphql, compose } from 'react-apollo';
-import { getAuthorsQuery, addBookMutation } from '../queries/queries'
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries'
 
 
 class AddBook extends Component {
@@ -37,7 +37,8 @@ class AddBook extends Component {
                 name: this.state.name,
                 genre: this.state.genre,
                 authorid: this.state.authorid
-            }
+            },
+            refetchQueries: [{query: getBooksQuery}]
         });
     }
 
