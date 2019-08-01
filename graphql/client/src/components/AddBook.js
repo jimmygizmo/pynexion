124cb13bf4a5774e0f2c7bace0b6c482ddc1ff18
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-08-01 00:04:34 -0700

    cleaned up form clearing work. started book details component.

--- graphql/client/src/components/AddBook.js
+++ graphql/client/src/components/AddBook.js
@@ -43,27 +43,15 @@ class AddBook extends Component {
     submitForm(e){
         e.preventDefault();  // Prevent default HTML form submission behavior.
         //console.log(this.state);
-        // Need to hold these state vars temporarily in local/function (var)
-        // scope so we can perform this.clearFormState() but still have the
-        // data available for the mutation. Since the mutation also includes
-        // the books query refetch with no way to wedge in the middle, we
-        // needed some way to clear the form even before the mutation+refetch.
+        // Temporarily hold form state so we can this.clearFormState()
         var name = this.state.name;
         var genre = this.state.genre;
         var authorid = this.state.authorid;
         this.clearFormState();
-        // RESULTS: The above works to clear the state variables BUT the form
-        // fields still show the values even though they are not added to
-        // a submission. Looks like we need to manually clear the form fields.
-        // FINAL FIX: We were never populating the form fields explicitly from
-        // the state. The browser was just retaining the data as a default
-        // behavior. Since we are in fact blanking out the state for those
-        // fields, the fix was to explicitly set the values into the form
-        // upon render. Will need to look out for problems, since we know
-        // this render is called upon every keypress, but of course, the state
-        // is also updated on every keypress too I think so it might work OK.
-        // Quick initial tests looks like it works.
-        
+        // Default broswer behavior was keeping data in form fields so we
+        // finally got the form to clear after a submit by explicitly writing
+        // state to the fields on each render, which also writes the cleared
+        // empty string values after a submit which is what we wanted.
 
         this.props.addBookMutation({
             variables: {
@@ -73,24 +61,6 @@ class AddBook extends Component {
             },
             refetchQueries: [{query: getBooksQuery}]
         });
-        // this.props.addBookMutation({
-        //     variables: {
-        //         name: this.state.name,
-        //         genre: this.state.genre,
-        //         authorid: this.state.authorid
-        //     },
-        //     refetchQueries: [{query: getBooksQuery}]
-        // });
-        // FAILED - Tried to clear form after a successful book addition by
-        // calling my new clearFormState method here, but it did not work and
-        // if you notice, it is re-fetching the query and thus re-rendering
-        // immediately after the mutation. And so the form is still populated
-        // with the recent data. We need another way. Wondering if there is a
-        // way to do it in the middle of the addBookMutation steps above. This
-        // mutation is doing the re-fetch as a built-in feature at the end of
-        // mutation so I have no idea how to get in the middle of that. There
-        // must be some other way.
-        // this.clearFormState();
     }
 
     // render() is called very frequently. It is called for every keypress for
@@ -103,17 +73,20 @@ class AddBook extends Component {
 
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text" value={this.state.name} onChange={ (e) => this.setState({name: e.target.value})} />
+                    <input type="text" value={this.state.name} onChange={
+                        (e) => this.setState({name: e.target.value})} />
                 </div>
 
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text" value={this.state.genre} onChange={ (e) => this.setState({genre: e.target.value})} />
+                    <input type="text" value={this.state.genre} onChange={
+                        (e) => this.setState({genre: e.target.value})} />
                 </div>
 
                 <div className="field">
                     <label>Author:</label>
-                    <select value={this.state.authorid} onChange={ (e) => this.setState({authorid: e.target.value})}>
+                    <select value={this.state.authorid} onChange={
+                        (e) => this.setState({authorid: e.target.value})}>
                         <option>Select Author</option>
                         {this.displayAuthors()}
                     </select>
---
+++ graphql/client/src/components/BookDetails.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+//import { getBooksQuery } from '../queries/queries'
+
