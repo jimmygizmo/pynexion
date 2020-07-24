32656df6e4dfd1ba4dfcc2151532d39d1b870840
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2020-07-24 00:37:17 -0700

    Supervisor in place now and working.

--- restproto/service/app.py
+++ restproto/service/app.py
@@ -28,6 +28,9 @@ if __name__ == '__main__':
     # 2. The container is run with the option mapping the port: -p 5000:5000 (docker run -p  5000:5000 <container_id>)
     # How this must runs when deployed on other platforms might vary slightly.
     app.run(debug=True, host="0.0.0.0")
+    # TODO: Enable control of the arguments passed to app.run() using env/config
+    # For production use we need debug=False and this should be controllable via env/config.
+    # Maybe Flask Script or something else within Flask or Flask-RESTplus has this covered already.
 
 
 ##
